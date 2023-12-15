f16cdfbbbb4934318b2f43ae329a7033ebad9723
VDA97 <victor.davi@hotmail.com>
2023-12-15 18:08:04 -0300

    Add Checkout Tests class

--- Default/Checkout.cpp
+++ Default/Checkout.cpp
@@ -1,15 +1,23 @@
 #include "Checkout.h"
 
-Checkout::Checkout(){
-    //
-};
+Checkout::Checkout() : total(0){};
 
 void Checkout::addItemPrice(std::string item, int price)
 {
-    // Todo
+    prices[item] = price;
 }
 
 void Checkout::addItem(std::string item)
+{
+    total += prices[item];
+}
+
+int Checkout::calculateTotal()
+{
+    return total;
+}
+
+void Checkout::addDiscount(std::string item, int nbrOfItems, int discountPrice)
 {
     // Todo
 }
--- Default/Checkout.h
+++ Default/Checkout.h
@@ -1,20 +1,22 @@
 #ifndef CHECKOUT_H_
 #define CHECKOUT_H_
 
-#include <vector>
+#include <map>
 #include <string>
-#include <algorithm>
-#include <stdexcept>
-#include <numeric>
 
 class Checkout
 {
-
 public:
     Checkout();
     void addItemPrice(std::string item, int price);
     void addItem(std::string item);
+    int calculateTotal();
+    void addDiscount(std::string item, int nbrOfItems, int discountPrice);
     virtual ~Checkout();
+
+protected:
+    std::map<std::string, int> prices;
+    int total;
 };
 
 #endif /*CHECKOUT_H_*/
