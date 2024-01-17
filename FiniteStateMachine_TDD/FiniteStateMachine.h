f7b178fbd4f837b5a63bf1af4cf3bf68ebb95eca
VDA97 <victor.davi@hotmail.com>
2024-01-17 18:56:05 -0300

    Add Project: FiniteStateMachine_TDD

---
+++ FiniteStateMachine_TDD/FiniteStateMachine.h
@@ -0,0 +1,9 @@
+#ifndef FINITESTATEMACHINE_H_
+#define FINITESTATEMACHINE_H_
+
+class FiniteStateMachine
+{
+    /*Todo -> Create a simple class for handling a timer*/
+};
+
+#endif /*FINITESTATEMACHINE_H_*/
